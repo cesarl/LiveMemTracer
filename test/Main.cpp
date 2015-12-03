7f5a3e728713986a27c93dcfb61b48e739c8aa53
Cesar Leblic <cesar.leblic@gmail.com>
2015-12-03 17:54:04 +0100

    Simple test

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,4 +1,16 @@
+#include "../Src/MemTracer.hpp"
+
 int main(int ac, char **av)
 {
+	void *a = LiveMemTracer::alloc(1);
+	void *b = LiveMemTracer::alloc(10);
+	void *c = LiveMemTracer::alloc(100);
+	void *d = LiveMemTracer::alloc(1000);
+
+	LiveMemTracer::dealloc(a);
+	LiveMemTracer::dealloc(b);
+	LiveMemTracer::dealloc(c);
+	LiveMemTracer::dealloc(d);
+
 	return 0;
 }
