c6e90f75c88b8ed5312922e5b8d9b36441d47e9a
Cesar Leblic <cesar.leblic@gmail.com>
2016-02-19 14:59:20 +0100

    Test modified

--- test/Main.cpp
+++ test/Main.cpp
@@ -63,7 +63,6 @@ static WorkerThread g_workerThread;
 #define LMT_CHUNK_NUMBER_PER_THREAD 4
 #define LMT_CACHE_SIZE 16
 #define LMT_DEBUG_DEV 1
-#define LMT_IMGUI 1
 #define LMT_IMGUI_INCLUDE_PATH "External/imgui/imgui.h"
 #define LMT_USE_MALLOC ::malloc
 #define LMT_USE_REALLOC ::realloc
@@ -112,12 +111,12 @@ static void error_callback(int error, const char* description)
 
 #if defined(OVERRIDE_NEW)
 //////////////////////////////////////////////////////////////////////////
-void *myMalloc(size_t size)
+inline void *myMalloc(size_t size)
 {
 	return LMT_ALLOC(size);
 }
 
-void myFree(void *ptr)
+inline void myFree(void *ptr)
 {
 	LMT_DEALLOC(ptr);
 }
@@ -325,7 +324,7 @@ int main(int ac, char **av)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "LiveMemTracer", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	gl3wInit();
 
@@ -357,19 +356,19 @@ int main(int ac, char **av)
 		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		Foo foo;
-		FooBarFunctor<1>::call(&foo);
-
-		FooBarFunctor<200>::call(foo.next);
-
-		Foo *p = foo.next;
-		while (p != nullptr)
-		{
-			auto *next = p->next;
-			delete[]p->ptr;
-			delete  p;
-			p = next;
-		}
+		//Foo foo;
+		//FooBarFunctor<1>::call(&foo);
+		//
+		//FooBarFunctor<200>::call(foo.next);
+		//
+		//Foo *p = foo.next;
+		//while (p != nullptr)
+		//{
+		//	auto *next = p->next;
+		//	delete[]p->ptr;
+		//	delete  p;
+		//	p = next;
+		//}
 
 		for (int i = 0; i < 100; ++i)
 		{
